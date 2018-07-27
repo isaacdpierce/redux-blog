73456df0595277ab15faf351cfeb8902d1da59d6
Isaac Pierce <contact@isaacdpierce.com>
2018-07-26 22:26:11 -0700

    Complete Action Creator to fetch requests from api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,15 @@
+import axios from 'axios';
 // Remember all actions must have a type
-const FETCH_POSTS = 'fetch_posts';
+export const FETCH_POSTS = 'fetch_posts';
+
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+const API_KEY = '?key=buddyboy9090';
 
 export function fetchPosts() {
+  const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+
  return {
-  type: FETCH_POSTS
+  type: FETCH_POSTS,
+  payload: request // because api request is assigned to payload: property it will automatically be resolved by the Redux middleware 
  };
 }
