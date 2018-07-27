21d8cd2cb636dbd31d2ee0558bcbc11fc2c1fba5
Isaac Pierce <contact@isaacdpierce.com>
2018-07-27 14:54:43 -0700

    Add input form and fields

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
-import React, { Field, reduxForm } from 'redux-form';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input
+          className="form-control"
+          type="text"
+          {...field.input} // adds all event handlers to the field (onChange,                   // onBlur, etc)
+        />
+      </div>
+    );  
+  }
+
   render() {
     return (
       <div>
-        PostsNew!
+        <form>
+          <Field 
+            label="Title"
+            name="title"
+            component={this.renderField}
+          />
+          <Field 
+            label="Tags"
+            name="tags"
+            component={this.renderField}
+          />
+        </form>
       </div>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm'
+})(PostsNew);
