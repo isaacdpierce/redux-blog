0baf3dd397a7a6dc0a69f4865db96de3f64690b3
Isaac Pierce <contact@isaacdpierce.com>
2018-07-27 21:12:52 -0700

    Wire up create post to remote api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 // Remember all actions must have a type
 export const FETCH_POSTS = 'fetch_posts';
+export const CREATE_POST = 'create_posts';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=buddyboy9090';
@@ -12,4 +13,13 @@ export function fetchPosts() {
   type: FETCH_POSTS,
   payload: request // because api request is assigned to payload: property it will automatically be resolved by the Redux middleware 
  };
+}
+
+export function createPost(values) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values);
+
+  return {
+    type: CREATE_POST,
+    payload: request
+  };
 }
